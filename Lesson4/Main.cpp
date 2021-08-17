ba42026e023d773dac173b9c2dd41730ffa4171c
Nakajima-Kuro <tnnamanh@gmail.com>
2021-08-17 18:25:24 +0700

    Remove debug lib at main.cpp

--- Lesson4/Main.cpp
+++ Lesson4/Main.cpp
@@ -1,12 +1,10 @@
 #include<iostream>
 #include"StateContext.h"
 #include"OpenState.h"
-#include <crtdbg.h>
 int main() {
 	{
 		StateContext* context = new StateContext(new LockState);
 		context->start();
 		delete context;
 	}
-	_CrtDumpMemoryLeaks();
 }
