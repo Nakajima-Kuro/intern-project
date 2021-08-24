0488fa9177601c9aa78e9cd91bf579e3b03bf54d
Nakajima-Kuro <tnnamanh@gmail.com>
2021-08-24 16:34:29 +0700

    Remove unused var in ArrowButton

--- MiniGameStarter/TrainingFramework/src/GameObject/ArrowButton.cpp
+++ MiniGameStarter/TrainingFramework/src/GameObject/ArrowButton.cpp
@@ -1,6 +1,6 @@
 #include "ArrowButton.h"
 ArrowButton::ArrowButton(std::shared_ptr<Model> model, std::shared_ptr<Shader> shader, std::pair<std::shared_ptr<Texture>, std::shared_ptr<Texture>> listTexture, int input)
-	: Sprite2D(-1, model, shader, listTexture.first), m_listTexture(listTexture), m_isHolding(false), m_input(input)
+	: Sprite2D(-1, model, shader, listTexture.first), m_listTexture(listTexture), m_input(input)
 {
 }
 
@@ -10,17 +10,11 @@ int ArrowButton::HandleKeyEvents(int key, bool bIsPressed)
 		if (bIsPressed)
 		{
 			// The button is being pressed down
-			m_isHolding = true;
 			this->SetTexture(m_listTexture.second);
 		}
 		else
 		{
-			if (m_isHolding == true)
-			{
-				this->SetTexture(m_listTexture.first);
-				m_isHolding = false;
-			}
-
+			this->SetTexture(m_listTexture.first);
 		}
 	}
 	return 0;
--- MiniGameStarter/TrainingFramework/src/GameObject/ArrowButton.h
+++ MiniGameStarter/TrainingFramework/src/GameObject/ArrowButton.h
@@ -4,12 +4,11 @@ class ArrowButton :
 	public Sprite2D
 {
 public:
-	ArrowButton() : Sprite2D(), m_listTexture({NULL, NULL}), m_isHolding(false), m_input(-1) {}
+	ArrowButton() : Sprite2D(), m_listTexture({NULL, NULL}), m_input(-1) {}
 	ArrowButton(std::shared_ptr<Model> model, std::shared_ptr<Shader> shader, std::pair<std::shared_ptr<Texture>, std::shared_ptr<Texture>> textureStatus, int input);
 	std::pair<std::shared_ptr<Texture>, std::shared_ptr<Texture>> m_listTexture;
 	int HandleKeyEvents(int key, bool bIsPressed);
 private:
 	int m_input;
-	bool m_isHolding;
 };
 
