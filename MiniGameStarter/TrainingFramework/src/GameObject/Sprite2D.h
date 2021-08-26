13a41798baf55efa8c572630cfccc029d5e08bbc
Nakajima-Kuro <tnnamanh@gmail.com>
2021-08-26 10:33:43 +0700

    Set visible to Sprite2D

--- MiniGameStarter/TrainingFramework/src/GameObject/Sprite2D.h
+++ MiniGameStarter/TrainingFramework/src/GameObject/Sprite2D.h
@@ -7,9 +7,10 @@ protected:
 	GLint		m_iWidth;
 	GLint		m_iHeight;
 	GLuint		m_vboId;
+	bool		m_isVisible;
 
 public:
-	Sprite2D() : BaseObject(), m_iWidth(0), m_iHeight(0), m_vboId(0) {}
+	Sprite2D() : BaseObject(), m_iWidth(0), m_iHeight(0), m_vboId(0), m_isVisible(true) {}
 	Sprite2D(GLint id, std::shared_ptr<Model> model, std::shared_ptr<Shader> shader, std::shared_ptr<Texture> texture);
 	Sprite2D(GLint id, std::shared_ptr<Model> model, std::shared_ptr<Shader> shader, Vector4 color);
 	Sprite2D(std::shared_ptr<Model> model, std::shared_ptr<Shader> shader, std::shared_ptr<Texture> texture);
@@ -22,5 +23,7 @@ public:
 	void		Set2DPosition(GLint x, GLint y);
 	void		Set2DPosition(Vector2 position);
 	void		SetSize(GLint width, GLint height);
+	void		SetVisible(bool isVisible);
+	bool		IsVisible();
 };
 
