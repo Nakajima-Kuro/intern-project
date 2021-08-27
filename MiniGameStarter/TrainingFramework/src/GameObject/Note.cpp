4377e932a512fd3e3ab85e717cc281c48e5345e4
Nakajima-Kuro <tnnamanh@gmail.com>
2021-08-27 18:14:07 +0700

    Finish Note

--- MiniGameStarter/TrainingFramework/src/GameObject/Note.cpp
+++ MiniGameStarter/TrainingFramework/src/GameObject/Note.cpp
@@ -1,35 +1,63 @@
 #include "Note.h"
 #include "ResourceManagers.h"
+#include "AnimationSprite2D.h"
 
-//Note::Note(float finish_y, float bpm, int difficulty, Vector2 position)
-//	:m_speed(0), m_spawn_y(0), m_finish_y(Globals::screenHeight), m_bpm(0), m_difficulty(4)
-//{
-//	//Init 3 arrow sprite
-//	auto model = ResourceManagers::GetInstance()->GetModel("Sprite2D.nfg");
-//	auto shader = ResourceManagers::GetInstance()->GetShader("AnimationShader");
-//
-//	auto texture = ResourceManagers::GetInstance()->GetTexture("Arrow/Left/spr_note.tga");
-//	
-//
-//	//Set note position
-//	this->SetPosition(position);
-//}
-//
-//Note::~Note()
-//{
-//	for (auto& element : m_listSprite) {
-//		delete element;
-//	}
-//	m_listSprite.clear();
-//}
-//
-//void Note::Update(float deltaTime)
-//{
-//	this->SetPosition(this->GetPosition().GetX(), this->GetPosition().GetY() + m_speed * deltaTime);
-//	Area2D::Update(deltaTime);
-//}
-//
-//void Note::Draw()
-//{
-//	Area2D::Draw();
-//}
+Note::Note()
+	: m_spawnLocation(Vector2()), m_finishLocation(Vector2()), m_speed(0), m_bpm(0), m_difficulty(4)
+{
+	Init();
+}
+
+Note::Note(Vector2 spawnPosition, Vector2 finishPosition, float bpm, int difficulty)
+	: m_spawnLocation(spawnPosition), m_finishLocation(finishPosition), m_speed(0), m_bpm(bpm), m_difficulty(difficulty)
+{
+	Init();
+}
+
+Note::~Note()
+{
+	delete m_sprite;
+}
+
+void Note::Init()
+{
+	//Init 3 arrow sprite
+	auto model = ResourceManagers::GetInstance()->GetModel("Sprite2D.nfg");
+	auto shader = ResourceManagers::GetInstance()->GetShader("AnimationShader");
+	auto texture = ResourceManagers::GetInstance()->GetTexture("Arrow/Left/spr_note.tga");
+	m_sprite = new AnimationSprite2D(model, shader, texture, 3, 0.0f);
+	m_sprite->SetPlayAnimation(false);
+	//Caculate speed
+	m_speed = float(m_finishLocation.y - m_spawnLocation.y) / (60.0f / m_bpm * m_difficulty);
+}
+
+void Note::Update(float deltaTime)
+{
+	if (Globals::screenHeight + m_sprite->GetHeight() < m_position.y) {
+		Destroy("");
+	}
+	else {
+		Set2DPosition(m_position.x, m_position.y + m_speed * deltaTime);
+		Area2D::Update(deltaTime);
+	}
+}
+
+void Note::Draw()
+{
+	m_sprite->Draw();
+	Area2D::Draw();
+}
+
+void Note::SetLane(int lane)
+{
+	m_sprite->setFrame(lane);
+}
+
+void Note::Destroy(std::string status)
+{
+	m_sprite->SetVisible(false);
+	if (strcmp(status.c_str(), "") != 0) {
+		//Being destroy by ArrowButton
+		//Emit Particles here
+	}
+}
--- MiniGameStarter/TrainingFramework/src/GameObject/Note.h
+++ MiniGameStarter/TrainingFramework/src/GameObject/Note.h
@@ -1,21 +1,29 @@
 #pragma once
 #include "Area2D.h"
-#include "Sprite2D.h"
+
+class AnimationSprite2D;
+
 class Note
 	: public Area2D
 {
 private:
+	AnimationSprite2D* m_sprite;
 	Vector2 m_spawnLocation;
 	Vector2 m_finishLocation;
 	float m_speed;
 	float m_bpm;
 	int m_difficulty;
-	AnimationSprite2D* m_sprite;
 public:
-	Note() : m_spawnLocation(Vector2()), m_finishLocation(Vector2()), m_speed(0), m_bpm(0), m_difficulty(4) {};
+	Note();
 	Note(Vector2 spawnPosition, Vector2 finishPosition, float bpm, int difficulty);
 	~Note();
+
+	//Game function
+	void Init();
 	void Update(float deltaTime) override;
 	void Draw() override;
+
+	void SetLane(int lane);
+	void Destroy(std::string status);
 };
 
